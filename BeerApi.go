4bc64425813b9938eb4728fa5df16aaabc161768
Simon Tucker <brotherlogic@gmail.com>
2017-08-18 06:51:03 -0700

    Fixed bomber get spec

--- BeerApi.go
+++ BeerApi.go
@@ -27,15 +27,15 @@ func (s *Server) GetDrunk(ctx context.Context, in *pb.Empty) (*pb.BeerList, erro
 	return list, nil
 }
 
-func authGet(ld *pb.Beer) bool {
+func authGet(ld *pb.Beer, r *pb.Beer) bool {
 	t := time.Now().Add(time.Hour * -24)
-	return ld.GetDrinkDate()-t.Unix() < 0 || ld.Size == "bomber"
+	return ld.GetDrinkDate()-t.Unix() < 0 || r.Size == "bomber"
 }
 
 //GetBeer gets a beer from the cellar
 func (s *Server) GetBeer(ctx context.Context, beer *pb.Beer) (*pb.Beer, error) {
 
-	if len(s.cellar.Drunk) > 0 && !authGet(s.cellar.Drunk[len(s.cellar.Drunk)-1]) {
+	if len(s.cellar.Drunk) > 0 && !authGet(s.cellar.Drunk[len(s.cellar.Drunk)-1], beer) {
 		return nil, errors.New("Unauthorized get")
 	}
 
