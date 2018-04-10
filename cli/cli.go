ecb7dc6d27d178e289fbd27054bfa8f73fd13934
Simon Tucker <brotherlogic@gmail.com>
2018-04-09 17:10:54 -0700

    Adds in on deck section

--- cli/cli.go
+++ cli/cli.go
@@ -37,9 +37,10 @@ func main() {
 	switch os.Args[1] {
 	case "list":
 		listFlags := flag.NewFlagSet("List", flag.ExitOnError)
+		var ondeck = listFlags.Bool("deck", false, "View on deck")
 		var cellar = listFlags.Int("cellar", 1, "Cellar to view")
 		if err := listFlags.Parse(os.Args[2:]); err == nil {
-			list, err := client.ListBeers(ctx, &pb.ListBeerRequest{})
+			list, err := client.ListBeers(ctx, &pb.ListBeerRequest{OnDeck: *ondeck})
 			if err == nil {
 				for i := 0; i < len(list.Beers); i++ {
 					for _, b := range list.Beers {
