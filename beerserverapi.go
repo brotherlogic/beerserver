4bf7f9c2bacb35f235ba0c5a7b43bc2a4eb3a44b
Simon Tucker <brotherlogic@gmail.com>
2021-03-04 10:22:36 -0800

    Better stash refresh

--- beerserverapi.go
+++ beerserverapi.go
@@ -178,7 +178,14 @@ func (s *Server) Update(ctx context.Context, req *pb.UpdateRequest) (*pb.UpdateR
 		return nil, err
 	}
 
-	if time.Now().Hour() < 14 && (time.Now().Weekday() == time.Monday || time.Now().Weekday() == time.Wednesday || time.Now().Weekday() == time.Friday) {
+	stashSize := 0
+	for _, cellar := range config.GetCellar().GetSlots() {
+		if cellar.Accepts == "stash" {
+			stashSize = len(cellar.GetBeers())
+		}
+	}
+
+	if time.Now().Hour() < 14 && (stashSize > 40 || (time.Now().Weekday() == time.Monday || time.Now().Weekday() == time.Wednesday || time.Now().Weekday() == time.Friday)) {
 		err = s.refreshStash(ctx, config)
 		if err != nil {
 			return nil, err
